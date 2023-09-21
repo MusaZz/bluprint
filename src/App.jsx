7377fda682b1d021a9d284753545f9068248465c
MusaZz <musa.agayev.21@gmail.com>
2023-09-21 11:11:30 +0400

    create sign-in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+//React Router
+import { Routes, Route } from "react-router-dom";
+
+//Pages
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SignIn />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes> */}
+    </div>
+  );
 };
 
 export default App;
