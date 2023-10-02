8671ada76f4524a37781af8148a861bc40dc4a0d
MusaZz <musa.agayev.21@gmail.com>
2023-10-02 07:07:47 +0400

    add dropdown menu for products navlink

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,6 +1,9 @@
 // React Router
 import { Link } from "react-router-dom";
 
+// Image
+import CapsHoverImage from "../assets/caps_hover_image.png";
+
 const NavLink = ({ link, dropdown }) => {
   return (
     <li
@@ -29,6 +32,41 @@ const NavLink = ({ link, dropdown }) => {
                 </Link>
               </li>
             </ul>
+          </div>
+        </div>
+      )}
+      {link === "Products" && (
+        <div className=" min-w-dropdown bg-white z-10 opacity-0 pointer-events-none translate-y-1 group-hover:opacity-100 group-hover:pointer-events-auto group-hover:translate-y-0 duration-200 absolute top-full -left-10 rounded-xl border border-gray-100 p-5 shadow">
+          <div className="flex gap-5">
+            <ul className="space-y-5 w-1/3">
+              <li className="text-xl font-bold">
+                <span className=" opacity-75 block hover:translate-x-2 hover:opacity-100 duration-200">
+                  Caps
+                </span>
+              </li>
+              <li className="text-xl font-bold">
+                <span className=" opacity-75 block hover:translate-x-2 hover:opacity-100 duration-200">
+                  Hoodies
+                </span>
+              </li>
+              <li className="text-xl font-bold">
+                <span className=" opacity-75 block hover:translate-x-2 hover:opacity-100 duration-200">
+                  T-shirts
+                </span>
+              </li>
+              <li className="text-xl font-bold">
+                <span className=" opacity-75 block hover:translate-x-2 hover:opacity-100 duration-200">
+                  Jeans
+                </span>
+              </li>
+            </ul>
+            <div className=" h-[241px] w-[515px] rounded-lg overflow-hidden">
+              <img
+                className="w-full h-full"
+                src={CapsHoverImage}
+                alt="caps"
+              />
+            </div>
           </div>
         </div>
       )}
