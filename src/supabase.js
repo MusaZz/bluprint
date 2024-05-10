1da4ef057fcdfa1d41a0aa90d42fa96141b6f265
Musa Aghayev <musa.agayev.21@gmail.com>
2024-05-11 03:05:33 +0400

    set privacy

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qrzyfunludqftyhhorhz.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyenlmdW5sdWRxZnR5aGhvcmh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3Mjg5NzgsImV4cCI6MjAxMzMwNDk3OH0.OAagQSpem8r1rti12lDfsvMPtm7g4ueof3A57rBC28Q";
+const supabaseUrl = BLUPRINT_SUPABASE_URL;
+const supabaseKey = process.env.BLUPRINT_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
